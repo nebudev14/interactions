5577621ac8b3692ff63834c6253b87b708830d24
NebuDev14 <wyun2006@gmail.com>
2022-06-16 22:06:46 -0400

    set up http routes to turn on led through http requests

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,28 @@
 use rust_gpiozero::*;
+use actix_web::{get, web, route, App, HttpServer, Responder};
 
-fn main() {
-    let mut led = LED::new(26);
-    led.blink(2.0, 3.0);
-    led.wait();
+
+#[get("/greet/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("hello {name}")
 }
+
+#[route("/led", method="GET")]
+async fn turn_led() -> impl Responder {
+    let mut led = LED::new(26);
+    led.toggle();
+    led.wait();
+    format!("Turned on LED")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(turn_led)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
